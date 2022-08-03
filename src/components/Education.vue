920c9db1a837ef2a53317a832d261ebb050965e9
linkovanne <linkovanne@yandex.ru>
2022-08-03 11:00:11 +0300

    deploy project

---
+++ src/components/Education.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Education"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Roadmap.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Roadmap"
+}
+</script>
+
+<style scoped>
+
+</style>
